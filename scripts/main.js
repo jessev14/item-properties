cfa7333311622f52737dfbfc75ae3ddbbeaa3f25
jessev14 <jessevo321@gmail.com>
2023-02-20 20:21:20 -0500

    add properties to more locations
    chat cards, item description OL
    also adds weapon support

--- scripts/main.js
+++ scripts/main.js
@@ -17,18 +17,19 @@ Hooks.once('init', () => {
         type: ItemPropertiesConfig,
         restricted: true
     });
+
+    libWrapper.register(moduleID, 'CONFIG.Item.documentClass.prototype.getChatData', newGetChatData, 'WRAPPER');
 });
 
 
 Hooks.on('renderItemSheet5e', async (app, [html], appData) => {
-    const item = app.object;
-    if (item.type === 'weapon') return;
-
     const detailsTab = html.querySelector('div.tab.details');
     if (!detailsTab) return;
 
+    const item = app.object;
     const flagData = item.getFlag(moduleID, 'itemProperties');
     const itemProperties = game.settings.get(moduleID, 'itemProperties');
+
     const itemPropertiesDiv = document.createElement('div');
     itemPropertiesDiv.classList.add('form-group', 'stacked', 'weapon-properties');
     itemPropertiesDiv.innerHTML = `<label>Item Properties</label>`;
@@ -40,6 +41,35 @@ Hooks.on('renderItemSheet5e', async (app, [html], appData) => {
             </label>
         `;
     }
-    
     detailsTab.prepend(itemPropertiesDiv);
+
+    const Ols =  html.querySelectorAll('ol.properties-list');
+    const propertiesOl = Ols?.[1] || Ols?.[0];
+    if (!propertiesOl || !flagData) return;
+
+    for (const [k,v ] of Object.entries(flagData)) {
+        if (!v) continue;
+        
+        const customProperty = itemProperties[k];
+        const propLi = document.createElement('li');
+        propLi.innerText = customProperty;
+        propertiesOl.appendChild(propLi);
+    }
 });
+
+async function newGetChatData(wrapped, htmlOptions = {}) {
+    const data = await wrapped(htmlOptions);
+
+    const itemProperties = game.settings.get(moduleID, 'itemProperties');
+    const flagData = this.getFlag(moduleID, 'itemProperties');
+    if (flagData) {
+        for (const [k,v ] of Object.entries(flagData)) {
+            if (!v) continue;
+            
+            const customProperty = itemProperties[k];
+            data.properties.push(customProperty);
+        }
+    }
+
+    return data;
+}
