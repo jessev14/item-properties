63a5be5e067462f22babcdcdc5cd74805c0817c1
jessev14 <jessevo321@gmail.com>
2025-03-04 14:37:26 -0500

    deprec

--- scripts/main.js
+++ scripts/main.js
@@ -21,15 +21,6 @@ Hooks.once('init', () => {
         type: ItemPropertiesConfig,
         restricted: true
     });
-
-    game.settings.register(moduleID, 'migrationV1.2.0', {
-        scope: 'world',
-        type: Boolean,
-        default: false
-    });
-
-    // libWrapper.register(moduleID, 'dnd5e.dataModels.ItemDataModel.prototype.getCardData', newGetCardData, 'WRAPPER');
-
 });
 
 Hooks.once('ready', async () => {
@@ -38,41 +29,4 @@ Hooks.once('ready', async () => {
         CONFIG.DND5E.itemProperties[prop] = { label: property.name };
         for (const [itemType, validSet] of Object.entries(CONFIG.DND5E.validProperties)) validSet.add(prop);
     }
-
-    if (game.settings.get(moduleID, 'migrationV1.2.0')) return;
-
-    const itemProperties = game.settings.get(moduleID, 'itemProperties');
-    const migratedData = {};
-    if (Object.entries(itemProperties).length) {
-        for (const [k, v] of Object.entries(itemProperties)) {
-            migratedData[k] = {
-                name: v,
-                tooltip: ''
-            };
-        }
-    } else return game.settings.set(moduleID, 'migrationV1.2.0', true);
-
-    await game.settings.set(moduleID, 'itemProperties', migratedData);
-    return game.settings.set(moduleID, 'migrationV1.2.0', true);
 });
-
-
-
-async function newGetCardData(wrapped, enrichmentOptions = {}) {
-    const context = await wrapped(enrichmentOptions);;
-
-    lg(context);
-    lg(this)
-
-    const item = this.parent;
-    const flagData = item.getFlag(moduleID, 'itemProperties');
-    const customProperties = game.settings.get(moduleID, 'itemProperties');
-    for (const [customProp, checked] of Object.entries(flagData)) {
-        if (!checked) continue;
-
-        const propLabel = customProperties[customProp]?.name;
-        if (propLabel) context.properties.push(propLabel);
-    }
-
-    return context;
-}
